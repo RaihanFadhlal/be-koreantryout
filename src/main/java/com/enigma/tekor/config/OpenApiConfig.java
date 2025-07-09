d077f61253ef845408953567b79b6838b8256cb4
Aflah Agus Rizkika <raaflahar@gmail.com>
2025-07-09 13:00:56 +0700

    feat: (aflah) add configuration Docker

--- src/main/java/com/enigma/tekor/config/OpenApiConfig.java
+++ src/main/java/com/enigma/tekor/config/OpenApiConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,23 +16,22 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         final String securitySchemeName = "bearerAuth";
-        
+
         return new OpenAPI()
+                .addServersItem(new Server().url("https://0a44046f54c6.ngrok-free.app").description("Production Server"))
+                .addServersItem(new Server().url("http://localhost:8081").description("Local Development Server"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
-                    new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                            new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                        )
-                )
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("TE-KOR API Documentation")
                         .description("Korean Language Try out App")
-                        .version("v1.0")
-                );
+                        .version("v1.0"));
     }
 }
