12dd10b33f54cd88db081ca488cb5644e8faa462
Raihan Fadhlal Aziz <raihanfadhlal@gmail.com>
2025-07-11 22:53:06 +0700

    fix update fullname validation

--- src/main/java/com/enigma/tekor/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/enigma/tekor/dto/request/UpdateProfileRequest.java
@@ -1,6 +1,7 @@
 package com.enigma.tekor.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,5 +15,7 @@ import lombok.Setter;
 @Builder
 public class UpdateProfileRequest {
     @NotBlank(message = "Full name is required")
+    @Pattern(regexp = "^[a-zA-Z\\s]{2,}$",
+            message = "Full name must be at least 2 characters, letters only (no numbers or symbols)")
     private String fullName;
 }
